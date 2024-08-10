d1d1e9018b28a81d4d508da798ef4946a047bbf2
NIHAT <NIHAT@DESKTOP-3F2LN5P>
2024-08-11 01:09:38 +0300

    Add project files.

---
+++ EncapsulationExample/Araba.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationExample
+{
+    internal class Araba
+    {
+
+        // Özelliklerimizden(Property) Kapı sayısı için kapsülleme işlemi yapmak istiyoruz.
+        // Eğer bir Araba nesnesi oluşturulduğunda Kapı Sayısı için 2 veya 4 dışında bir değer atanmak isterse konsol ekranında bir uyarı mesajı yayınlayalım ve Kapı Sayısı değerini -1 olarak atayalım.
+
+
+        private string _brand;
+        private string _model;
+        private string _color;
+        private int _doorCount;
+
+        public string Brand
+        {
+            get
+            {
+                return _brand;
+            }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    _brand = value;
+
+                else
+                    Console.WriteLine("Arabanın markası boş olamaz.");
+
+            }
+        }
+
+        public string Model
+        {
+            get
+            {
+                return _model;
+            }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    _model = value;
+
+                else
+                    Console.WriteLine("Arabanın modeli boş olamaz.");
+            }
+        }
+
+        public string Color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    _color = value;
+                else
+                    Console.WriteLine("Arabanın rengi boş olamaz.");
+            }
+        }
+
+        public int DoorCount
+        {
+            get
+            {
+                return _doorCount;
+            }
+            set
+            {
+                if (value == 2 || value == 4)
+                {
+
+                    _doorCount = value;
+                }
+                else
+                {
+                    _doorCount = -1;
+                    Console.WriteLine("Kapı sayınız 2 yada 4 değil\n");
+                }
+            }
+        }
+
+        public Araba()
+        {
+
+        }
+
+        public Araba(string brand, string model, string color, int doorCount)
+        {
+            Brand = brand;
+            Model = model;
+            Color = color;
+            DoorCount = doorCount;
+
+        }
+
+        public void TellInfo()
+        {
+            
+                Console.WriteLine($"Arabanın markası : {_brand}\nArabanın modeli : {_model}\nArabanın rengi : {_color}\nArabanın kapı sayısı : {_doorCount}");
+        }
+    }
+}
---
+++ EncapsulationExample/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace EncapsulationExample
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+
+            Araba newCar1 = new Araba();
+            Console.WriteLine("Default Constructor ile oluşturulan boş nesne");
+            newCar1.TellInfo();
+
+            Console.WriteLine("----------------------------------------");
+
+            Araba newCar2 = new Araba();
+            Console.WriteLine("\nDefault Constructor ile oluşturulan 3 kapılı nesne");
+            newCar2.Brand = "";
+            newCar2.Model = "";
+            newCar2.Color = "";
+            newCar2.DoorCount = 3;
+            newCar2.TellInfo();
+
+            Console.WriteLine("----------------------------------------");
+
+            Araba newCar3 = new Araba();
+            Console.WriteLine("\nDefault Constructor ile oluşturulan 2 kapılı nesne");
+            newCar3.Brand = "Nissan";
+            newCar3.Model = "Juke";
+            newCar3.Color = "Gri";
+            newCar3.DoorCount = 2;
+            newCar3.TellInfo();
+
+            Console.WriteLine("----------------------------------------");
+
+            Araba newCar4 = new Araba();
+            Console.WriteLine("\nDefault Constructor ile oluşturulan 4 kapılı nesne");
+            newCar4.Brand = "Kia";
+            newCar4.Model = "Sportage";
+            newCar4.Color = "Siyah";
+            newCar4.DoorCount = 4;
+            newCar4.TellInfo();
+
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine("\nParametre alan Constructor ile oluşturulan 3 kapılı nesne");
+            Araba newCar5 = new Araba("", "", "", 3);            
+            newCar5.TellInfo();
+
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine("\nParametre alan Constructor ile oluşturulan 2 kapılı nesne");
+            Araba newCar6 = new Araba("Peugeot","RCZ","Siyah",2);            
+            newCar6.TellInfo();
+
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine("\nParametre alan Constructor ile oluşturulan 4 kapılı nesne");
+            Araba newCar7 = new Araba("Togg", "T10X", "Kırmızı", 4);
+            newCar7.TellInfo();
+        }
+    }
+}
