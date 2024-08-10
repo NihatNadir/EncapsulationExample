6545599360d05724dac39133c4112e546531c13c
Nihat Nadir Börlü <134305591+NihatNadir@users.noreply.github.com>
2024-08-11 01:21:50 +0300

    Program.cs

--- EncapsulationExample/Program.cs
+++ EncapsulationExample/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 namespace EncapsulationExample
 {
     class Program
@@ -6,12 +6,14 @@ namespace EncapsulationExample
         public static void Main(string[] args)
         {
            
-
+            // Default Constructor ile boş nesne oluşturma
             Araba newCar1 = new Araba();
             Console.WriteLine("Default Constructor ile oluşturulan boş nesne");
             newCar1.TellInfo();
 
             Console.WriteLine("----------------------------------------");
+
+            // Default Constructor ile nesne oluşturma
 
             Araba newCar2 = new Araba();
             Console.WriteLine("\nDefault Constructor ile oluşturulan 3 kapılı nesne");
@@ -23,6 +25,8 @@ namespace EncapsulationExample
 
             Console.WriteLine("----------------------------------------");
 
+            // Default Constructor ile nesne oluşturma
+
             Araba newCar3 = new Araba();
             Console.WriteLine("\nDefault Constructor ile oluşturulan 2 kapılı nesne");
             newCar3.Brand = "Nissan";
@@ -32,6 +36,8 @@ namespace EncapsulationExample
             newCar3.TellInfo();
 
             Console.WriteLine("----------------------------------------");
+
+            // Default Constructor ile nesne oluşturma
 
             Araba newCar4 = new Araba();
             Console.WriteLine("\nDefault Constructor ile oluşturulan 4 kapılı nesne");
@@ -43,17 +49,23 @@ namespace EncapsulationExample
 
             Console.WriteLine("----------------------------------------");
 
+            // Parametre alan Constructor ile nesne oluşturma
+
             Console.WriteLine("\nParametre alan Constructor ile oluşturulan 3 kapılı nesne");
             Araba newCar5 = new Araba("", "", "", 3);            
             newCar5.TellInfo();
 
             Console.WriteLine("----------------------------------------");
 
+            // Parametre alan Constructor ile nesne oluşturma
+
             Console.WriteLine("\nParametre alan Constructor ile oluşturulan 2 kapılı nesne");
             Araba newCar6 = new Araba("Peugeot","RCZ","Siyah",2);            
             newCar6.TellInfo();
 
             Console.WriteLine("----------------------------------------");
+
+            // Parametre alan Constructor ile nesne oluşturma
 
             Console.WriteLine("\nParametre alan Constructor ile oluşturulan 4 kapılı nesne");
             Araba newCar7 = new Araba("Togg", "T10X", "Kırmızı", 4);
